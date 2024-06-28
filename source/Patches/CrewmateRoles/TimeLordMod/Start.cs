5e99ac57f9c1e6b3d02754267c6acb40d686f8b6
zwykl0soba <wgwiktora@outlook.com>
2024-06-28 22:52:19 +0200

    Add files via upload

--- source/Patches/CrewmateRoles/TimeLordMod/Start.cs
+++ source/Patches/CrewmateRoles/TimeLordMod/Start.cs
@@ -1,17 +1,19 @@
-using System;
+﻿using System;
 using HarmonyLib;
 using TownOfUs.Roles;
 
 namespace TownOfUs.CrewmateRoles.TimeLordMod
 {
-    [HarmonyPatch(typeof(IntroCutscene._CoBegin_d__19), nameof(IntroCutscene._CoBegin_d__19.MoveNext))]
+    // Upewnij się, że poprawnie odnajdujesz typ wewnętrzny
+    
     public static class Start
     {
-        public static void Postfix(IntroCutscene._CoBegin_d__19 __instance)
+        // Zaktualizuj sygnaturę metody zgodnie z faktycznym typem
+        public static void Postfix(IntroCutscene __instance)
         {
             foreach (var role in Role.GetRoles(RoleEnum.TimeLord))
             {
-                var TimeLord = (TimeLord) role;
+                var TimeLord = (TimeLord)role;
                 TimeLord.FinishRewind = DateTime.UtcNow;
                 TimeLord.StartRewind = DateTime.UtcNow;
                 TimeLord.FinishRewind = TimeLord.FinishRewind.AddSeconds(CustomGameOptions.InitialCooldowns - CustomGameOptions.RewindCooldown);
